380f83b9edcc7f58dea2fe64e34db3fed4bae372
Serena Graham <serenaelizabeth@hotmail.co.uk>
2021-04-30 16:10:13 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import firebase from 'firebase/app';
-import React, { FC } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import './App.css';
-import Dashboard from './Components/Dashboard';
-import FormPage from './Components/FormPage';
-import { useFirebaseContext } from './Utils/firebaseContext';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import firebase from "firebase/app";
+import React, { FC } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./App.css";
+import Dashboard from "./Components/Dashboard";
+import FormPage from "./Components/FormPage";
+import { useFirebaseContext } from "./Utils/firebaseContext";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import styled from "styled-components";
 
 const App: FC = () => {
   const { auth } = useFirebaseContext();
@@ -18,19 +19,37 @@ const App: FC = () => {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <Switch>
-            <Route path="/form">{user && <FormPage />}</Route>
-            <Route path="/">
-              {user ? <Dashboard /> : <button onClick={signIn}>Sign in</button>}
-            </Route>
-          </Switch>
-        </Router>
-      </header>
-    </div>
+    <header className="App-header">
+      <Router>
+        <Navbar>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/form">Add New Plant</Link>
+          </li>
+        </Navbar>
+        <Switch>
+          <Route path="/form">{user && <FormPage />}</Route>
+          <Route path="/">
+            {user ? <Dashboard /> : <button onClick={signIn}>Sign in</button>}
+          </Route>
+        </Switch>
+      </Router>
+    </header>
   );
 };
 
 export default App;
+
+const Navbar = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  background: rgba(255, 255, 255, 0.52);
+  align-items: center;
+  list-style: none;
+  position: fixed;
+  top: 0;
+`;
