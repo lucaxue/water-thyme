66b9c57b2c8e432b27e2460e45e2a3e2361499a2
Serena Graham <serenaelizabeth@hotmail.co.uk>
2021-04-30 15:07:39 +0100

    styled components

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,17 +1,33 @@
-import React, { FC } from 'react';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { useFirebaseContext } from '../Utils/firebaseContext';
+import React, { FC } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import styled from "styled-components";
+import { useFirebaseContext } from "../Utils/firebaseContext";
+import PlantCard from "./PlantCard";
 
 const Dashboard: FC = () => {
   const { auth, firestore } = useFirebaseContext();
-  const plantsRef = firestore.collection('plants');
+  const plantsRef = firestore.collection("plants");
 
-  const query = plantsRef.where('uid', '==', auth?.currentUser?.uid);
+  const query = plantsRef.where("uid", "==", auth?.currentUser?.uid);
 
-  const [plants] = useCollectionData(query, { idField: 'id' });
+  const [plants] = useCollectionData(query, { idField: "id" });
   console.log(plants);
 
-  return <div>hi you are logged in</div>;
+  return (
+    <div>
+      <CardsWrapper>
+        {plants &&
+          plants.map((plant) => <PlantCard key={plant.id} data={plant} />)}
+      </CardsWrapper>
+    </div>
+  );
 };
 
 export default Dashboard;
+
+const CardsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  min-width: 80vw;
+`;
